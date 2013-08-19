3fd17ef1107936034173d16704c211f5f40e2544
Sam Aaron <samaaron@gmail.com>
2013-08-19 16:34:04 +0100

    initial commit

---
+++ init-defining-pi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install supercollider ruby1.9.1 libqscintilla2-8 git libqscintilla2-dev libqt4-dev
+
+su pi
+
+mkdir ~/bin
+mkdir -p ~/defining-pi
+touch ~/defining-pi/name.txt
+mkdir ~/code
+
+rm -rf ~/defining-pi
+cd ~
+git clone https://github.com/samaaron/defining-pi.git
+
+cd ~/bin
+git clone https://github.com/samaaron/defining-pi-bin.git defining-pi
+chmod u+x ~/bin/defining-pi/*.sh
---
+++ update-defining-pi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+su pi
+cd ~/bin/defining-pi
+git pull
